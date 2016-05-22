f22734a4438392f77c222c7f8b599f8e131f6ef3
mcramet <maxence.cramet@tritondigital.com>
2016-05-22 10:13:14 -0400

    Only clean tables in tests

--- src/test/java/com/mct/appdirect/HealthTest.java
+++ src/test/java/com/mct/appdirect/HealthTest.java
@@ -2,21 +2,13 @@ package com.mct.appdirect;
 
 import com.mct.appdirect.utils.IntegrationTest;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 public class HealthTest extends IntegrationTest {
-
-    @Value("${local.server.port}")
-    private int port;
-
-    private final RestTemplate template = new TestRestTemplate();
 
     @Test
     public void shouldReturnOk() {
