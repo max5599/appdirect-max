6252d473ff9ca957d5303d0ae2f983d545f02ebf
mcramet <maxence.cramet@tritondigital.com>
2016-05-22 12:04:41 -0400

    Change code of internal error to unknown error

--- src/main/java/com/mct/appdirect/response/ErrorResponseBuilder.java
+++ src/main/java/com/mct/appdirect/response/ErrorResponseBuilder.java
@@ -3,12 +3,12 @@ package com.mct.appdirect.response;
 public class ErrorResponseBuilder {
 
     public final static String USER_ALREADY_EXISTS = "USER_ALREADY_EXISTS";
-    private static final String INTERNAL_ERROR = "INTERNAL_ERROR";
+    private static final String UNKNOWN_ERROR = "UNKNOWN_ERROR";
 
     private ErrorResponseBuilder() {}
 
     public static ErrorResponse internalErrorResponse() {
-        return aFailureResponseWithErrorCode(INTERNAL_ERROR);
+        return aFailureResponseWithErrorCode(UNKNOWN_ERROR);
     }
 
     public static ErrorResponse aFailureResponseWithErrorCode(String errorCode) {
