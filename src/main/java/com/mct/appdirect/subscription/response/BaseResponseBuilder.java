560529ebdc832fe9a8a30311c4fbde2a2e92baa0
mcramet <maxence.cramet@tritondigital.com>
2016-05-26 18:46:47 -0400

    Make constructor of BaseResponseBuilder private

--- src/main/java/com/mct/appdirect/subscription/response/BaseResponseBuilder.java
+++ src/main/java/com/mct/appdirect/subscription/response/BaseResponseBuilder.java
@@ -2,6 +2,9 @@ package com.mct.appdirect.subscription.response;
 
 public class BaseResponseBuilder {
 
+    private BaseResponseBuilder() {
+    }
+
     public static BaseResponse aSuccessfulResponse() {
         return new BaseResponse(true);
     }
